f1e873a902e12f44f77a7e930d5fabaf5d880dfc
Azhdan Yulmi <azhdan.yulmi@nrg-bg.com>
2024-01-28 12:31:20 +0200

    Initial commit

---
+++ src/main/java/com/dost/bookmanagementapi/BookmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.dost.bookmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dost/bookmanagementapi/BookmanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dost.bookmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
